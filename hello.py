518b4eb6e8ec9fa7115292b9aa35d47c671a42a3
Rahul <connectthapa84@gmail.com>
2022-07-17 11:11:53 -0400

    added initial files

---
+++ hello.py
@@ -0,0 +1,13 @@
+def addthis(x,y):
+    #import pdb;pdb.set_trace()
+    print(f"This is x: {x} and the x-type {type(x)}")
+    print(f"This is y: {y} and the y-type is {type(y)}")
+    
+    try: 
+        result = x+y
+    except TypeError:
+        print("The wrong type passed")
+        result = int(x)+int(y)
+    
+    print(f"This is the result: {result}")
+    return result
