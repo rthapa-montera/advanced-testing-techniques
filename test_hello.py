f844bc2b1eba86fdab108dad61c49c07df6c2de3
Rahul Thapa <rthapa@monterahealth.com>
2022-07-17 15:41:31 +0000

    added cli tool

--- test_hello.py
+++ test_hello.py
@@ -1,10 +1,20 @@
 import pytest
 from hello import addthis
+from click.testing import CliRunner
+from hello_cli import calladdthis
 
 def test_addthis():
     assert 3 == addthis(1,2)
+
 
 def test_addthis_wrong_type():
     with pytest.raises(ValueError) as excinfo:
         addthis("one",2)
     assert "invalid literal" in str(excinfo.value)
+    
+
+def test_cli():
+  runner = CliRunner()
+  result = runner.invoke(calladdthis, ['--x', '1', '--y', '1'])
+  assert result.exit_code == 0
+  assert '2' in result.output
